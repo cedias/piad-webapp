bff815da07121db45c2b9aa2971229d43e05a243
Charles-Emmanuel <charles-emmanuel.dias@etu.upmc.fr>
2014-03-25 14:58:25 +0100

    solene

--- app.js
+++ app.js
@@ -11,14 +11,27 @@ var connection = mysql.createConnection({
 
 connection.query('USE amazon');
 
+
 app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+app.set('view engine', 'mustache');
+app.engine('mustache', require('hogan-middleware').__express);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){ 
-    res.render('index');
+
+	connection.query('SELECT r.review_id as rid, u.username as uid, p.product_name as pname, r.honesty_score as hon FROM reviews r,users u, products p where u.user_id = r.user_id AND p.product_id = r.product_id ORDER BY honesty_score asc LIMIT 0,10 ', function(err, rows, fields) {
+	  if (err) throw err;
+	   res.render('index',{title:"SpamReviewDetection",result:rows});
+	});
+
 });
 
+
+
+
 app.listen(app.get('port'));
+
 console.log('Express server listening on port ' + app.get('port'));
